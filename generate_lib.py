c86aff58ecc7090d6f0c45a47eb4057292f20c60
Priyanka Raina <praina@stanford.edu>
2020-12-11 16:49:21 -0800

    Fix paths to which lib is copied.

--- generate_lib.py
+++ generate_lib.py
@@ -1,5 +1,4 @@
 from shutil import copyfile
 from skywater_path import *
 
-copyfile(SKYWATER130_HOME + '/libraries/sky130_fd_sc_hd/latest/timing/sky130_fd_sc_hd__tt_025C_1v80.lib', 'view-standard/stdcells.lib')
-copyfile(SKYWATER130_HOME + '/libraries/sky130_fd_sc_hd/latest/timing/sky130_fd_sc_hd__tt_025C_1v80.lib', 'generate_db/sky130_fd_sc_hd__tt_025C_1v80.lib')
+copyfile(SKYWATER130_HOME + '/libraries/sky130_fd_sc_hd/latest/timing/sky130_fd_sc_hd__tt_025C_1v80.lib', 'generate_lib/sky130_fd_sc_hd__tt_025C_1v80.lib')
