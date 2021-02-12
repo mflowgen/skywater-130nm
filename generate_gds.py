e8cf658f09127bd10ce4645fed3e79e6f9f0190f
Priyanka Raina <praina@stanford.edu>
2021-02-11 17:43:35 -0800

    Update gds generation.

--- generate_gds.py
+++ generate_gds.py
@@ -2,14 +2,19 @@ from shutil import copyfile
 from glob import glob
 from utils import *
 from skywater_path import *
+import os 
 
 cell_dirs = glob(SKYWATER130_HOME + "/libraries/sky130_fd_sc_hd/latest/cells/*/")
+
+if(~os.path.isdir("view-standard/stdcells.gds")):
+    os.mkdir("view-standard/stdcells.gds")
 
 infilenames = []
 
 for cell_dir in cell_dirs:
     libname = cell_dir.split('/')[-5]
     cellname = cell_dir.split('/')[-2]
+
 
     infilenames = infilenames + glob(cell_dir+libname+'__'+cellname+'_*.gds')
     
