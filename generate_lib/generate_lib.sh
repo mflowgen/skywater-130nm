11f5055db1d38a093f7dc8db9cd268b26bfef22d
Hansem Ro <hansemro@outlook.com>
2023-03-11 13:08:24 -0800

    generate_lib: remove 288 fix
    It seems newer open_pdks and volare builds have already implemented this
    particular hotfix. Applying this hotfix again yields invalid results, so
    let's remove it.
    
    Tested with:
    - open_pdks 1.0.367
    - volare sky130 build: e6f9c8876da77220403014b116761b0b2d79aab4

--- generate_lib/generate_lib.sh
+++ generate_lib/generate_lib.sh
@@ -6,17 +6,6 @@ export lib_file=$TOP/generate_lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 # cp $SKYWATER130_HOME/libs.ref/sky130_fd_sc_hd/lib/sky130_fd_sc_hd__tt_025C_1v80.lib $lib_file
 python $TOP/generate_lib/generate_lib.py
 
-# Hotfix for issue #288
-# Swap related_bias_pin VPB and VNB
-sed -i 's/related_bias_pin : "VPB";/related_bias_pin : "VNBTEMP";/g' $lib_file
-sed -i 's/related_bias_pin : "VNB";/related_bias_pin : "VPB";/g' $lib_file
-sed -i 's/related_bias_pin : "VNBTEMP";/related_bias_pin : "VNB";/g' $lib_file
-
-# Swap pg_type nwell and pwell
-sed -i 's/pg_type : "nwell";/pg_type : "pwelltemp";/g' $lib_file
-sed -i 's/pg_type : "pwell";/pg_type : "nwell";/g' $lib_file
-sed -i 's/pg_type : "pwelltemp";/pg_type : "pwell";/g' $lib_file
-
 # Hotfix for issue #183
 sed -i '82150 a \
         pg_pin ("VNB") {\
