a55598edbd48e9ac750f20f37e8e48a279da53f8
Hansem Ro <hansemro@outlook.com>
2023-03-11 15:28:48 -0800

    generate_lib: automate hotfix for 183
    Automated hotfix for 183 in preparation for processing other timing
    corners.

--- generate_lib/generate_lib.sh
+++ generate_lib/generate_lib.sh
@@ -6,43 +6,35 @@ export lib_file=$TOP/generate_lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 # cp $SKYWATER130_HOME/libs.ref/sky130_fd_sc_hd/lib/sky130_fd_sc_hd__tt_025C_1v80.lib $lib_file
 python $TOP/generate_lib/generate_lib.py
 
-# Hotfix for issue #183
-sed -i '82150 a \
-        pg_pin ("VNB") {\
-            pg_type : "pwell";\
-            physical_connection : "device_layer";\
-            voltage_name : "VNB";\
-        }' $lib_file
-sed -i '82289 a \
-        pg_pin ("VNB") {\
-            pg_type : "pwell";\
-            physical_connection : "device_layer";\
-            voltage_name : "VNB";\
-        }' $lib_file
-sed -i '82428 a \
-        pg_pin ("VNB") {\
-            pg_type : "pwell";\
-            physical_connection : "device_layer";\
-            voltage_name : "VNB";\
-        }' $lib_file
-sed -i '82710 a \
-        pg_pin ("VNB") {\
-            pg_type : "pwell";\
-            physical_connection : "device_layer";\
-            voltage_name : "VNB";\
-        }' $lib_file
-sed -i '82849 a \
-        pg_pin ("VNB") {\
-            pg_type : "pwell";\
-            physical_connection : "device_layer";\
-            voltage_name : "VNB";\
-        }' $lib_file
-sed -i '82988 a \
-        pg_pin ("VNB") {\
-            pg_type : "pwell";\
-            physical_connection : "device_layer";\
-            voltage_name : "VNB";\
-        }' $lib_file
+process_lib() {
+    # Hotfix for issue #183
+    lines=$(grep -n 'sky130_fd_sc_hd__lpflow_lsbuf_lh_hl_isowell_tap' $1 | cut -f1 -d:)
+    offset=0
+    for line in $lines; do
+        line=$(($line + 22 + offset))
+        sed -i "$line a \\
+        pg_pin (\"VNB\") {\\
+            pg_type : \"pwell\";\\
+            physical_connection : \"device_layer\";\\
+            voltage_name : \"VNB\";\\
+        }" $1
+        offset=$(($offset + 5))
+    done
+    offset=0
+    lines=$(grep -n 'sky130_fd_sc_hd__lpflow_lsbuf_lh_isowell_tap' $1 | cut -f1 -d:)
+    for line in $lines; do
+        line=$(($line + 26 + offset))
+        sed -i "$line a \\
+        pg_pin (\"VNB\") {\\
+            pg_type : \"pwell\";\\
+            physical_connection : \"device_layer\";\\
+            voltage_name : \"VNB\";\\
+        }" $1
+        offset=$(($offset + 5))
+    done
+}
+
+process_lib $lib_file
 
 lc_shell -f lc_shell_gen_lib.tcl
 
